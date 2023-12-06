c770b048cd792276e50fcf9eb822ef1899a71f2d
Ender2397 <131037469+Ender2397@users.noreply.github.com>
2023-12-06 23:40:08 +0000

    ;-;

---
+++ Darkmode.js
@@ -0,0 +1,9 @@
+
+function DarkMode() {
+    var body = document.body;
+    body.classList.toggle('dark-mode');
+
+    var navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('navbar-dark');
+    navbar.classList.toggle('navbar-light');
+}
